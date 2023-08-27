d644f728f90b8bff70437b6f18aaaf8abc699fbb
rababni-essaly <golam.rabbani@essaly.com>
2023-08-27 15:20:14 +0300

    fdg

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/found', (req, res)=>{
     res.send("Hello Worlddd");
 })
 
-app.get('/fin-all', (req, res)=>{
+app.get('/fin-al', (req, res)=>{
     res.send("Hello final test Success");
 })
 
