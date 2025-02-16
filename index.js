2a6d4cf4499a643c12ec97d92eb45ea4ecb833bb
golamrabbani3587 <golamrabbani35873587@gmail.com>
2025-02-16 11:16:28 +0600

    up

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-require('dotenv')
+require("dotenv");
 
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res)=>{
-    res.send("Hello World");
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.get('/found', (req, res)=>{
-    res.send("Hello Worlddd");
-})
+app.get("/found", (req, res) => {
+  res.send("Hello Worlddd");
+});
 
-app.get('/fin-all', (req, res)=>{
-    res.send("Hello final test Success");
-})
+app.get("/fin-all", (req, res) => {
+  res.send("Hello final test Success");
+});
 
-app.get('/fin-all', (req, res)=>{
-    res.send("Hello final test Success");
-})
+app.get("/fin-all", (req, res) => {
+  res.send("Hello final test Success");
+});
 
-app.get('/test-final', (req, res)=>{
-    res.send('Ready to test kubernetes')
-})
+app.get("/test-final", (req, res) => {
+  res.send("Ready to test kubernetes");
+});
 
-app.get('/test-kube', (req, res)=>{
-    res.send('kubernetes running')
-})
+app.get("/test-kub", (req, res) => {
+  res.send("kubernetes running");
+});
 
-
-app.listen(process.env.PROD_PORT,()=>{
-   console.log('process.env.PROD_PORT');
-})
-module.exports =  app;
+app.listen(process.env.PROD_PORT, () => {
+  console.log("process.env.PROD_PORT");
+});
+module.exports = app;
